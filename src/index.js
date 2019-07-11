70ab4f8aa02b4e44efc03c5380c6274564e403e4
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-11 21:01:12 +0200

    new structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import ImageList from "./ImageList";
-import SearchBar from "./SearchBar";
-
-class App extends Component {
-  render() {
-    return <div>Hello World</div>;
-  }
-}
+import ImageList from "./components/ImageList";
+import SearchBar from "./components/SearchBar";
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
