95fe801ea3ab9359c1ea14780915618814d4831d
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-11 21:13:01 +0200

    minor fix

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <form>
-          <div className="ui icon input">
-            <input type="text" placeholder="Search..." />
-            <i className="inverted circular search link icon" />
-          </div>
-        </form>
+        <SearchBar />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,16 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   state = {};
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div>
+        <form>
+          <div className="ui icon input">
+            <input type="text" placeholder="Search..." />
+            <i className="inverted circular search link icon" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
