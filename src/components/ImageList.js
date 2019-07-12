cd3d8c37ace633960bc530fc9a6524a42d51dde6
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-12 21:13:19 +0200

    added ImageCard

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import "./ImageList.css";
 import React, { Component } from "react";
+import ImageCard from "./ImageCard";
 
 class ImageList extends Component {
   render() {
-    const images = this.props.images.map(({ id, urls, description }) => {
-      return <img key={id} src={urls.regular} alt={description} />;
+    const images = this.props.images.map(image => {
+      return <ImageCard key={image.id} image={image} />;
     });
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
   }
 }
 
